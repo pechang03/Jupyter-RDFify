78823fafe74ee111b0a42b118107af3fcfd84229
AwesomeDeepAI <63055320+AwesomeDeepAI@users.noreply.github.com>
2022-12-13 16:12:38 +0100

    Update util.py

--- jupyter-rdfify/util.py
+++ jupyter-rdfify/util.py
@@ -1,16 +1,13 @@
 import argparse
 import re
 
-
 class StopCellExecution(Exception):
     """Special exception which can be raised to stop the execution of the cell without visible error."""
 
     def _render_traceback_(self):
         pass
 
-
 class MagicParser(argparse.ArgumentParser):
-
     def exit(self, status=0, message=None):
         if status:
             print("Parser exited with error: {}".format(message))
@@ -20,7 +17,6 @@ class MagicParser(argparse.ArgumentParser):
         print("Error: {}".format(message))
         self.exit()
 
-
 def literal_to_string(literal, lang=None, datatype=None):
     lit = f"\"{literal}\""
     if lang is not None:
@@ -28,7 +24,6 @@ def literal_to_string(literal, lang=None, datatype=None):
     if datatype is not None:
         lit += f"^^{datatype}"
     return lit
-
 
 def strip_comments(text):
     """Special comment strip function for formats which do not support comments (e.g. json)"""
