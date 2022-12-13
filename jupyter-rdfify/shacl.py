f88f65a183ff334b6ffb81233c74c37fcd09ffcd
AwesomeDeepAI <63055320+AwesomeDeepAI@users.noreply.github.com>
2022-12-13 15:06:17 +0100

    Update shacl.py

--- jupyter-rdfify/shacl.py
+++ jupyter-rdfify/shacl.py
@@ -1,5 +1,6 @@
 from pyshex.utils.schema_loader import SchemaLoader
 from .rdf_module import RDFModule
+from .graph import draw_graph, parse_graph
 from pyshacl import validate
 from rdflib import Graph
 
@@ -9,7 +10,7 @@ class SHACLModule(RDFModule):
         self.parser.add_argument(
             "action", choices=["parse", "validate", "prefix"], help="Action to perform")
         self.parser.add_argument(
-            "--shape", "-s", help="Shape label for referencing")
+            "--label", "-l", help="Shape label for referencing")
         self.parser.add_argument(
             "--graph", "-g", help="Graph label for validation")
         
@@ -23,47 +24,58 @@ class SHACLModule(RDFModule):
         self.serialize_report_graph = "turtle"
         self.prefix = ""
 
+    def check_label(self, label, store):
+        if label is not None:
+            if label in store["rdfgraphs"]:
+                return True
+            else:
+                self.log(f"Graph labelled '{label}' not found.")
+        else:
+            self.log(
+                "Please specify the label of a graph with parameter --label or -l.")
+            
     def print_result(self, result):
         self.log(f"Evaluating the shape!")
         if result.result:
             self.logger.print("Test PASSED!")
         else:
             self.logger.print(f"Test FAILED! Reason:\n{result.reason}\n")
-
-    def handle(self, params, store):
-        if params.action == "prefix":
-            self.prefix = params.cell + "\n"
-            self.log("Prefixes have been stored!")
             
-        elif params.action == "parse":
-            if params.cell is not None:
-                try:
-                    input_graph = params.graph
-                    self.g_to_parse.parse(data=input_graph, format=self.graph_fotmat, encoding=self.graph_encoding)                    
-                    self.log("Shape successfully parsed!")
-                except Exception as e:
-                    self.log(f"Error during shape parse:\n{str(e)}")
-            else:
-                self.log("No cell content to parse.")
-        elif params.action == "validate":
-            if params.label is not None and params.graph is not None:
-                if params.label in store["shapes_graph"]:
-                    if params.graph in store["data_graph"]:
-                        result = self.evaluate(
-                            store["rdfgraphs"][params.graph],
-                            store["rdfshapes"][params.label],
-                            data_graph_format,
-                            shacl_graph_format, 
-                            inference,
-                            debug,
-                            serialize_report_graph,
-                        )
-                        for r in result:
-                            self.print_result(r)
+    def handle(self, params, store):
+        if params.action is not None:
+            if params.action == "prefix":
+                self.prefix = params.cell + "\n"
+                self.log("Prefixes have been stored!")            
+
+            elif params.action == "parse":
+                if self.check_label(params.label, store):
+                    parse_graph(store["rdfgraphs"][params.label], self.logger, "ttl")
+                 else:
+                    self.log("No cell content to parse.")
+                    
+            elif params.action == "draw":
+                if self.check_label(params.label, store):
+                    draw_graph(store["rdfgraphs"][params.label], self.logger)
+                    
+            elif params.action == "validate":
+                if params.label is not None and params.graph is not None:
+                    if params.label in store["shapes_graph"]:
+                        if params.graph in store["data_graph"]:
+                            result = self.evaluate(
+                                store["rdfgraphs"][params.graph],
+                                store["rdfshapes"][params.label],
+                                data_graph_format,
+                                shacl_graph_format, 
+                                inference,
+                                debug,
+                                serialize_report_graph,
+                            )
+                            for r in result:
+                                self.print_result(r)
+                        else:
+                            self.log(
+                                f"Found no data graph! '{params.graph}'.")
                     else:
-                        self.log(
-                            f"Found no data graph! '{params.graph}'.")
+                        self.log(f"Found no shapes graph! '{params.shape}'.")
                 else:
-                    self.log(f"Found no shapes graph! '{params.shape}'.")
-            else:
-                self.log("A shapes graph and a data graph are required for the validation!")
+                    self.log("A shapes graph and a data graph are required for the validation!")
